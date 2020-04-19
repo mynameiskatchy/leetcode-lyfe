28cb4b00f02a290f32de3f5d3d6f2478ff7a368c
mynameiskatchy <karchietrinh@gmail.com>
2020-04-19 10:00:16 -0400

    Finish numJewelsInStones_771

--- numJewelsInStones_771.py
+++ numJewelsInStones_771.py
@@ -12,6 +12,7 @@ Example 1:
 
 Input: J = "aA", S = "aAAbbbb"
 Output: 3
+
 Example 2:
 
 Input: J = "z", S = "ZZ"
@@ -29,7 +30,29 @@ class Solution(object):
         :type J: str
         :type S: str
         :rtype: int
+
+        Runtime: 12 ms, faster than 96.07 % of Python online submissions for Jewels and Stones.
+        Memory Usage: 12.9 MB, less than 5.38 % of Python online submissions for Jewels and Stones.
+
         """
+        
+        counter = 0
+        for i in S:
+            if i in J:
+                counter += 1
+        return counter
+
+
 
 if __name__ == "__main__":
+    J1, S1, ans1 = "aA", "aAAbbbb", 3
+    J2, S2, ans2 = "z", "ZZ", 0
+
+    s = Solution()
+    out1 = s.numJewelsInStones(J1, S1)
+    out2 = s.numJewelsInStones(J2, S2)
+
+    print(out1)
+    print(out2)
+
     pass
