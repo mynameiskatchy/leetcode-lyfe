ee1553de6d421f6a024115c89c69e91ce69f30c6
mynameiskatchy <karchietrinh@gmail.com>
2020-04-23 15:02:16 -0400

    Finish decompressRLElist_1313

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # leetcode-lyfe
 To record my leetcode progress
 
-<https://dev.to/irfnhm/how-to-set-python3-as-a-default-python-version-on-mac-4jjf>
-<https://leetcode.com/explore/interview/card/top-interview-questions-easy/>
-
 April 2020
 1342. Number of Steps to Reduce a Number to Zero (numberOfSteps)
 1108. Defanging an IP Address (defangIPaddr)
 771. Jewels and Stones
 1313. Decompress Run-Length Encoded List
+
+<https://dev.to/irfnhm/how-to-set-python3-as-a-default-python-version-on-mac-4jjf>
+<https://linuxize.com/post/change-git-commit-message/>
+<https://leetcode.com/explore/interview/card/top-interview-questions-easy/>
+
--- decompressRLElist_1313.py
+++ decompressRLElist_1313.py
@@ -17,6 +17,7 @@ Output: [2,4,4,4]
 Explanation: The first pair [1,2] means we have freq = 1 and val = 2 so we generate the array [2].
 The second pair [3,4] means we have freq = 3 and val = 4 so we generate [4,4,4].
 At the end the concatenation [2] + [4,4,4] is [2,4,4,4].
+
 Example 2:
 
 Input: nums = [1,1,2,3]
@@ -36,8 +37,34 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: List[int]
+
+        Runtime: 52 ms, faster than 86.43% of Python online submissions for Decompress Run-Length Encoded List.
+        Memory Usage: 12.9 MB, less than 100.00% of Python online submissions for Decompress Run-Length Encoded List.
         """
 
+        decomp_list = []
+        rest = nums[:]   # copy nums list for manipulation
+
+        for i in range(int(len(rest)/2)):
+            pair = rest[0:2]   # [freq, val] pair
+            decomp_list.extend([pair[1]] * pair[0])   # expand [freq, val] pair
+            rest = rest[2:]    # remaining pairs
+
+        return decomp_list
+
 if __name__ == "__main__":
-    
-    pass
+    in1, out1 = [1, 2, 3, 4], [2, 4, 4, 4]
+    in2, out2 = [1, 1, 2, 3], [1, 3, 3]
+    in3, out3 = [11, 5, 9, 2, 8, 1], [11, 11, 11, 11, 11, 9, 9, 8]
+    in4 = [55, 11, 70, 26, 62, 64]
+
+    s = Solution()
+    # a = s.decompressRLElist(in1)
+    # b = s.decompressRLElist(in2)
+    c = s.decompressRLElist(in3)
+
+    # print(a)
+    # print(b)
+    print(c)
+
+    pass      
