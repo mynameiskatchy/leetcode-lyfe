d8f28c15f9bdfde99e810bafa62c081ba2f000dc
mynameiskatchy <karchietrinh@gmail.com>
2020-05-05 21:53:44 -0400

    Set up 1431. Kids With the Greatest Number of Candies (kidsWithCandies)

--- destCity_1436.py
+++ destCity_1436.py
@@ -5,7 +5,7 @@ destCity()
 <https://leetcode.com/problems/destination-city/>
 ====================================================================================
 
-You are given the array paths, where paths[i] = [cityAi, cityBi] 
+You are given the array paths, where paths[i] = [cityA_i, cityB_i] 
 means there exists a direct path going from cityAi to cityBi. 
 Return the destination city, that is, 
 the city without any path outgoing to another city.
