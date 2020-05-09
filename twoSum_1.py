80df5138f8e327289c889e26d8d058a4b8ffae8c
mynameiskatchy <karchietrinh@gmail.com>
2020-05-09 01:21:26 -0400

    Set up 1. Two Sum (twoSum)

---
+++ twoSum_1.py
@@ -0,0 +1,36 @@
+
+"""
+====================================================================================================
+1. Two Sum
+twoSum()
+<https://leetcode.com/problems/two-sum/>
+<https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/546/>
+====================================================================================================
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+--------
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+"""
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        return 0
+
+    
+if __name__ == "__main__":
+    pass
