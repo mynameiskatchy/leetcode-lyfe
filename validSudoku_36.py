fabfd62ba881d36bbbd543b5948734cb9f0769cd
mynameiskatchy <karchietrinh@gmail.com>
2020-06-04 23:31:00 -0400

    First attempt for validSudoku_36

--- validSudoku_36.py
+++ validSudoku_36.py
@@ -1,5 +1,5 @@
 
-"""
+""" Could be improved way more
 ====================================================================================================
 36. Valid Sudoku
 validSudoku()
@@ -7,7 +7,7 @@ validSudoku()
 <https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/769/>
 ====================================================================================================
 
-Determine if a 9x9 Sudoku board is valid. 
+Determine if a 9x9 Sudoku board is valid.
 Only the filled cells need to be validated according to the following rules:
 
 1. Each row must contain the digits 1-9 without repetition.
@@ -49,7 +49,7 @@ Input:
   [".",".",".",".","8",".",".","7","9"]
 ]
 Output: false
-Explanation: Same as Example 1, except with the 5 in the top left corner being 
+Explanation: Same as Example 1, except with the 5 in the top left corner being
     modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
 
 Note:
@@ -67,10 +67,79 @@ class Solution(object):
         """
         :type board: List[List[str]]
         :rtype: bool
+
+        470 / 504 test cases passed.
         """
 
-        return 0
+        m = {}
+        n = {}
 
-    
+        for i in range(9):
+            col = []
+            for row in board:
+                col.append(row[i])
+            m[i] = board[i]
+            n[i] = col
+
+        def sum_list(values):
+            s = 0
+            for i in values:
+                if i != '.':
+                    s += int(i)
+            return s
+
+        m = [sum_list(i) == sum_list(set(i)) for i in m.values()]
+        n = [sum_list(i) == sum_list(set(i)) for i in n.values()]
+
+        if False in (m + n):
+            return False
+        else:
+            return True
+
+
 if __name__ == "__main__":
+    input1 = \
+        [
+            ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+            ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+            [".", "9", "8", ".", ".", ".", ".", "6", "."],
+            ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+            ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+            ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+            [".", "6", ".", ".", ".", ".", "2", "8", "."],
+            [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+            [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+        ]
+
+    input2 = \
+        [
+            ["8","3",".",".","7",".",".",".","."],
+            ["6",".",".","1","9","5",".",".","."],
+            [".","9","8",".",".",".",".","6","."],
+            ["8",".",".",".","6",".",".",".","3"],
+            ["4",".",".","8",".","3",".",".","1"],
+            ["7",".",".",".","2",".",".",".","6"],
+            [".","6",".",".",".",".","2","8","."],
+            [".",".",".","4","1","9",".",".","5"],
+            [".",".",".",".","8",".",".","7","9"]
+        ]
+
+    input3 = \
+        [
+            [".",".",".",".","5",".",".","1","."],
+            [".","4",".","3",".",".",".",".","."],
+            [".",".",".",".",".","3",".",".","1"],
+            ["8",".",".",".",".",".",".","2","."],
+            [".",".","2",".","7",".",".",".","."],
+            [".","1","5",".",".",".",".",".","."],
+            [".",".",".",".",".","2",".",".","."],
+            [".","2",".","9",".",".",".",".","."],
+            [".",".","4",".",".",".",".",".","."]]
+
+    s = Solution()
+    a = s.isValidSudoku(input1)
+    b = s.isValidSudoku(input2)
+    c = s.isValidSudoku(input3)
+    
+    print(a, b, c)
     pass
