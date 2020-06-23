f4ab4dc55a3cb59be830be51c61dd2127e7d0e53
mynameiskatchy <karchietrinh@gmail.com>
2020-06-23 04:58:40 -0400

    Finish deleteNode_237

--- deleteNode_237.py
+++ deleteNode_237.py
@@ -32,10 +32,10 @@ The given node will not be the tail and it will always be a valid node of the li
 Do not return anything from your function.
 """
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution(object):
@@ -43,7 +43,32 @@ class Solution(object):
         """
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
+        
+        Runtime: 32 ms, faster than 40.35% of Python online submissions for Delete Node in a Linked List.
+        Memory Usage: 13.3 MB, less than 23.70% of Python online submissions for Delete Node in a Linked List.
+        
+        Runtime: 28 ms, faster than 70.29% of Python online submissions for Delete Node in a Linked List.
+        Memory Usage: 13.3 MB, less than 33.80% of Python online submissions for Delete Node in a Linked List.
+        
+        Runtime: 24 ms, faster than 90.42% of Python online submissions for Delete Node in a Linked List.
+        Memory Usage: 13.2 MB, less than 69.43% of Python online submissions for Delete Node in a Linked List.
         """
+        node.val = node.next.val
+        node.next = node.next.next
+
+        
+
 
 if __name__ == "__main__":
+    head = [4, 5, 1, 9]
+    head = [ListNode(i) for i in head]
+
+    for i in range(len(head)-1):
+        head[i].next = head[i + 1]
+        
+    node1 = 5
+    node2 = 1
+
+
+
     pass
