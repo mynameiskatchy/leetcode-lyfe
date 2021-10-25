c3dc4ffbe3769ccd73e028b9b70d7bc12b8b49ba
mynameiskatchy <karchietrinh@gmail.com>
2021-10-24 21:45:52 -0400

    Add comments to HackerRank

--- HackerRank/plus_minus.py
+++ HackerRank/plus_minus.py
@@ -2,15 +2,16 @@
 class Solution(object):
 
     def plus_minus(self, n, arr):
+
         # something to track the values we want to record
         # positive, negative, zero
-
         count_dict = {
             -1: 0,
             0: 0,
             1: 0
         }
 
+        # logic to loop through and count
         for val in arr:
             if val < 0:
                 count_dict[-1] += 1
@@ -19,11 +20,12 @@ class Solution(object):
             else:
                 count_dict[1] += 1
 
+        # logic to display outputs
         for i in [1, -1, 0]:
             str = '{:.6f}'.format(round(count_dict[i] / n, 6))
             print(str)
             
-            
+        # Want to return 'not a number'    
         return None
 
 if __name__ == "__main__":
