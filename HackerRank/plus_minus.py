fedb9386c1120655219931aa9974ee49fbd01fe5
mynameiskatchy <karchietrinh@gmail.com>
2021-10-24 21:42:53 -0400

    Add plusminus

---
+++ HackerRank/plus_minus.py
@@ -0,0 +1,34 @@
+
+class Solution(object):
+
+    def plus_minus(self, n, arr):
+        # something to track the values we want to record
+        # positive, negative, zero
+
+        count_dict = {
+            -1: 0,
+            0: 0,
+            1: 0
+        }
+
+        for val in arr:
+            if val < 0:
+                count_dict[-1] += 1
+            elif val == 0:
+                count_dict[0] += 1
+            else:
+                count_dict[1] += 1
+
+        for i in [1, -1, 0]:
+            str = '{:.6f}'.format(round(count_dict[i] / n, 6))
+            print(str)
+            
+            
+        return None
+
+if __name__ == "__main__":
+    a = Solution()
+    arr = [-4, 3, -9, 0, 4, 1]
+    n = 6
+
+    a.plus_minus(n, arr)
